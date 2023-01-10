9ba61e508d6dd618583ae21c862d2049c37477cc
Володя Андрейчук <pow3r@Volodas-MacBook-Air.local>
2023-01-10 20:58:02 +0200

    add packages && update eslint

---
+++ src/store/index.ts
@@ -0,0 +1,9 @@
+import { createStore, createLogger } from 'vuex'
+
+const debug = process.env.NODE_ENV !== 'production'
+
+export default createStore({
+  modules: {},
+  strict: debug,
+  plugins: debug ? [createLogger()] : [],
+})
